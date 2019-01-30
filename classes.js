e3a34a4a1880a2560b757ef9ca46dd668159ffc3
Matias Perez <matias.perezferrero@gmail.com>
2019-01-30 15:06:35 -0700

    Completed thru problem 3

--- classes.js
+++ classes.js
@@ -29,6 +29,20 @@
   Call your class Employee and receive all the data in the constructor in the order listed above.
 */
 
+class Employee {
+  constructor(first_name, last_name, email, age) {
+  this.first_name = first_name;
+  this.last_name = last_name;
+  this.email = email;
+  this.age = age
+  }
+  
+  makeWidget() {
+   return this.first_name+" "+this.last_name+" Widget" 
+  }
+}
+
+new Employee("Matias", "Perez", "blah@gmail", 25);
 //Code Here
 
 
@@ -49,7 +63,27 @@
   Call your new class Manager
 */
 
-//Code Here
+class Manager {
+  constructor(first_name, last_name, email, age) {
+  this.first_name = first_name;
+  this.last_name = last_name;
+  this.email = email;
+  this.age = age;
+  this.reports = [];
+  }
+  
+  makeWidget() {
+   return this.first_name+" "+this.last_name+" Widget" 
+  }
+
+  hire(employee) {
+    this.reports.push(employee);
+  }
+
+  fire(index) {
+    this.reports.splice(index, 1)
+  }
+}//Code Here
 
 
 
@@ -75,6 +109,43 @@
   Call your new class ProgressiveManager
 */
 
+class ProgressiveManager {
+  constructor(first_name, last_name, email, age) {
+  this.first_name = first_name;
+  this.last_name = last_name;
+  this.email = email;
+  this.age = age;
+  this.reports = [];
+  this.title = "Not a manager";
+  this.bonus = 0
+  }
+  
+  makeWidget() {
+   return this.first_name+" "+this.last_name+" Widget" 
+  }
+
+hire(employee) {
+  this.reports.push(employee);
+  if(this.reports.length >= 101){
+    this.title = "Bestest Manager"
+  } else if(this.reports.length >=51){
+    this.title = "Manager Plus"
+  } else if(this.reports.length >= 11){
+    this.title = "Manager"
+  } else if(this.reports.length >=4){
+    this.title = "Mostly Manager"
+  } else if(this.reports.length >=1){
+    this.title = "Barely Manager"
+  } else if(this.reports.length == 0){
+    this.title = "Not a manager"
+  }
+}
+
+  fire(index) {
+    this.reports.splice(index, 1)
+    this.bonus += 100
+  }
+}
 //Code Here
 
 
